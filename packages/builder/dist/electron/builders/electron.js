99aa56579fdbc8d4e22604060113e9f2f9178868
Mengting Yan <mengting.yan1@ibm.com>
2021-06-01 14:42:11 -0400

    chore: remove staging directory from electron build.sh

--- packages/builder/dist/electron/builders/electron.js
+++ packages/builder/dist/electron/builders/electron.js
@@ -47,14 +47,54 @@ const { basename, join } = require('path')
 const packager = require('electron-packager')
 const { copy, emptyDir } = require('fs-extra')
 const { createReadStream, createWriteStream, readdir } = require('fs')
+const { exec } = require('child_process')
 
 const sign = require('./sign')
 const notarize = require('./notarize')
 
 const nodePty = 'node-pty-prebuilt-multiarch'
 
+async function buildWebpack(buildPath) {
+  if (process.env.KUI_HEADLESS_WEBPACK) {
+    console.log('Building headless bundles via webpack')
+    await new Promise((resolve, reject) => {
+      exec(
+        `MODE=production CLIENT_HOME="${buildPath}" KUI_STAGE="${buildPath}" npx --no-install webpack-cli --mode=production --config "${buildPath}/node_modules/@kui-shell/webpack/headless-webpack.config.js"`,
+        (err, stdout, stderr) => {
+          console.log('stdout', stdout)
+          if (err) {
+            console.error(err)
+            reject(stderr)
+          } else {
+            resolve()
+          }
+        }
+      )
+    })
+  }
+
+  console.log('Building electron bundles via webpack')
+  await new Promise((resolve, reject) => {
+    exec(
+      `TARGET=electron-renderer MODE=production CLIENT_HOME="${buildPath}" KUI_STAGE="${buildPath}" npx --no-install webpack-cli --mode=production --config "${buildPath}/node_modules/@kui-shell/webpack/webpack.config.js"`,
+      (err, stdout, stderr) => {
+        console.log('stdout', stdout)
+        if (err) {
+          console.error(err)
+          reject(stderr)
+        } else {
+          resolve()
+        }
+      }
+    )
+  })
+}
+
 /** afterCopy hook to copy in the platform-specific node-pty build */
-async function copyNodePty(buildPath, electronVersion, targetPlatform, targetArch, callback) {
+async function build(buildPath, electronVersion, targetPlatform, targetArch, callback) {
+  console.log('buildPath', buildPath)
+  await buildWebpack(buildPath)
+  console.log('copying node pty', process.platform, targetPlatform, targetArch, osArch())
   if (process.platform === targetPlatform && targetArch === osArch()) {
     // if the current platform matches the target platform, there is
     // nothing to do
@@ -62,6 +102,7 @@ async function copyNodePty(buildPath, electronVersion, targetPlatform, targetArc
   } else {
     const target = `${targetPlatform}-${targetArch}`
     const sourceDir = join(process.env.BUILDER_HOME, 'dist/electron/vendor', nodePty, 'build', target, 'electron')
+    console.log('sourceDir', sourceDir)
 
     readdir(sourceDir, async (err, files) => {
       if (err) {
@@ -164,9 +205,10 @@ function package(baseArgs /*: { dir: string, name: string, platform: string, arc
     },
 
     // lifecycle hooks to copy in our extra bits
-    afterCopy: [copyNodePty, copySignableBits(baseArgs)]
+    afterCopy: [build, copySignableBits(baseArgs)]
   })
 
+  console.error('args', args)
   if (process.env.APP_BUNDLE_ID) {
     // this part of electron-packager seems weird; we need to set the
     // macOS bundleID here (i.e. HERE ALSO!! we of course need to pass
